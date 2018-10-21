1d659da91d71fe61c3f4830eb4064d701293165e
echoyoung46 <vickyyoung0406@yeah.net>
2018-10-21 23:02:28 +0800

    update

--- js/game.js
+++ js/game.js
@@ -33,6 +33,7 @@ game.states.menu = function () {
 
 game.states.start = function () {
 	this.preload = function () {
+			this.scale = 1;
 			this.score = 0;
 			this.gameOver = false;
 			this.speed = -300;
@@ -46,12 +47,12 @@ game.states.start = function () {
 			this.angela = this.add.sprite(0, 0, 'angela')
 			game.physics.arcade.enable(this.angela);
 			this.angela.anchor.set(0.5);
+			this.angela.width = 150;
+			this.angela.height = 150;
 			this.angela.x = this.angela.width;
 			this.angela.y = game.height - this.angela.height / 2;
 			this.angela.body.collideWorldBounds = true;
 			game.camera.follow(this.angela);
-			this.angela.scale.x = 0.5;
-			this.angela.scale.y = 0.5;
 
 			this.skyGroup = game.add.group();
 			this.skyGroup.enableBody = true;
@@ -60,18 +61,27 @@ game.states.start = function () {
 			var sky3 = this.add.sprite(0, 0, 'colorDragon');
 			sky1.visible = sky2.visible = sky3.visible = false;
 			sky1.alive = sky2.alive = sky3.alive = false;
+
+			sky1.width = 80;
+			sky1.height = 50;
+			// sky2.width = 80;
+			// sky2.height = 50;
+			// sky3.width = 80;
+			// sky3.height = 50;
+
 			this.skyGroup.add(sky1);
-			this.skyGroup.add(sky2);
-			this.skyGroup.add(sky3);
+			// this.skyGroup.add(sky2);
+			// this.skyGroup.add(sky3);
 			sky1.body.setCircle(sky1.width / 2);
-			sky2.body.setCircle(sky2.width / 2);
-			sky3.body.setCircle(sky3.width / 2);
+			// sky2.body.setCircle(sky2.width / 2);
+			// sky3.body.setCircle(sky3.width / 2);
 			this.skyGroup.setAll('checkWorldBounds', true);
 			this.skyGroup.setAll('outOfBoundsKill', true);
-			this.skyGroup.scale.x = 0.2;
-			this.skyGroup.scale.y = 0.2;
 
 			game.time.events.loop(1000, this.addProps, this);
+
+			this.cursors = game.input.keyboard.createCursorKeys();
+			this.signal = game.input.onDown;
 		},
 		this.gameStart = function () {
 			game.state.start('start');
@@ -82,26 +92,40 @@ game.states.start = function () {
 			game.state.start('start');
 		},
 		this.update = function () {
-			// this.land.tilePosition.x -= 1;
+			this.signal.add(function (e) {
+
+				if (e.clientX < game.width/2) {
+					this.angela.y = this.angela.height;
+					// this.angela.scale.x = -1;
+				} else {
+					this.angela.y = game.height - this.angela.height / 2;
+					// this.angela.scale.x = 1;
+				}
+
+				if (e.clientY < game.height / 3 * 2 && this.angela.body.onFloor()) {
+					this.angela.y = -800;
+				}
+
+			}, this);
 		},
 		this.addProps = function () { //添加道具
 			if (this.gameOver) {
 				return;
 			}
-		
+
 			var random = game.rnd.between(1, 100);
 			if (this.score >= 100) { //分数大于300后再随机道具里添加乌鸦
 				var small = this.skyGroup.getFirstDead();
 				if (small !== null) {
 					// this.angela.x = this.angela.width;
 					// this.angela.y = game.height - this.angela.height / 2;
-					small.reset(game.width, game.height - small.height);
+					small.reset(game.width, game.height - this.angela.height / 2);
 					small.body.velocity.x = this.speed;
 				}
 			} else {
 				var small = this.skyGroup.getFirstDead();
 				if (small !== null) {
-					small.reset(game.width, game.height - small.height);
+					small.reset(game.width, game.height - this.angela.height / 2);
 					small.body.velocity.x = this.speed;
 				}
 			}
